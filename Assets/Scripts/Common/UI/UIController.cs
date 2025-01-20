4493e68a67aa89fbf671e3495ce821a0a90b63a2
ChuChuMyo <ben0310@naver.com>
2025-01-21 01:25:39 +0900

    0.0.6
    스탯, 스킬 탭

--- Assets/Scripts/Common/UI/UIController.cs
+++ Assets/Scripts/Common/UI/UIController.cs
@@ -5,11 +5,25 @@ using UnityEngine;
 public class UIController : MonoBehaviour
 {
     
-    private void OnClickInventoryBtn()
+    public void OnClickInventoryBtn()
     {
         //일단 열려있는 UI를 닫음
         UIManager.Instance.CloseAllOpenUI();
         var uiData = new BaseUIData();
         UIManager.Instance.OpenUI<InventoryUI>(uiData);
+    }
+
+    public void OnClickSkillBtn()
+    {
+        UIManager.Instance.CloseAllOpenUI();
+        var uiData = new BaseUIData();
+        UIManager.Instance.OpenUI<SkillUI>(uiData);
+    }
+
+    public void OnClickStatusBtn()
+    {
+        UIManager.Instance.CloseAllOpenUI();
+        var uiData = new BaseUIData();
+        UIManager.Instance.OpenUI<StatusUI>(uiData);
     }
 }
